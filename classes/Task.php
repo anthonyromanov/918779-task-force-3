aa8d7784c55b192b3d26d9e78eff9ddf8ab4eb45
Anton Romanov <609990@gmail.com>
2022-01-11 17:13:14 +0300

    Правки

--- classes/Task.php
+++ classes/Task.php
@@ -13,70 +13,99 @@ class Task
     const ACTION_DONE = 'done';
     const ACTION_REFUSED = 'refused';
 
+    const EXECUTOR = 'executor';
+    const CUSTOMER = 'customer';
+
     private $idCustomer;
     private $idExecutor;
 
     public $currentStatus;
     public $currentAction;
 
-    public function __construct($idExecutor, $idCustomer = null)
-    {
+    public static $mapStatuses =
+    
+    [
+        self::STATUS_NEW => 'Новое',
+        self::STATUS_CANCELED => 'Отменено',
+        self::STATUS_WORKING => 'В работе',
+        self::STATUS_DONE => 'Выполнено',
+        self::STATUS_FAILED => 'Провалено'
+    
+    ];
+
+    public static $mapActions = [
+    
+        self::ACTION_CANCEL => 'Отменить задание',
+        self::ACTION_RESPOND => 'Откликнуться на задание',
+        self::ACTION_DONE => 'Задание выполнено',
+        self::ACTION_REFUSED => 'Отказаться от задания'
+    
+    ];
+
+    public function __construct($idExecutor, $idCustomer = null) {
+        
         $this->idCustomer = $idCustomer;
         $this->idExecutor = $idExecutor;
+    
     }
 
-    public function getMapStatuses()
-    {
-        $mapStatuses =
-        [
-            self::STATUS_NEW => 'Новое',
-            self::STATUS_CANCELED => 'Отменено',
-            self::STATUS_WORKING => 'В работе',
-            self::STATUS_DONE => 'Выполнено',
-            self::STATUS_FAILED => 'Провалено'
-        ];
-
+    public static function getMapStatuses() {
+    
         return $mapStatuses;
+    
     }
 
-    public function getMapActions()
-    {
-        $mapActions =
-        [
-            self::ACTION_CANCEL => 'Отменить задание',
-            self::ACTION_RESPOND => 'Откликнуться на задание',
-            self::ACTION_DONE => 'Задание выполнено',
-            self::ACTION_REFUSED => 'Отказаться от задания'
-        ];
+    public static function getMapActions() {
+        
         return $mapActions;
+
     }
 
-    public function getStatus()
-    {
+    public function getStatus() {
+
         return $this->currentStatus;
+    
     }
 
-    public function getAvailableActions($role)
-    {
-        if ($role === 'customer') {
+    public function getAvailableActions($role) {
+
+        if ($role === CUSTOMER) {
+
             if ($this->currentStatus === STATUS_NEW) {
+            
                 return self::ACTION_CANCEL;
+            
             }
+            
             if ($this->currentStatus === STATUS_WORKING) {
+            
                 return self::ACTION_DONE;
+            
             }
+            
             return null;
         }
-        if ($role === 'executor') {
+
+        if ($role === EXECUTOR) {
+        
             if ($this->currentStatus === STATUS_NEW) {
+        
                 return self::ACTION_RESPOND;
+            
             }
+        
             if ($this->currentStatus === STATUS_WORKING) {
+        
                 return self::ACTION_REFUSED;
+     
             }
+     
             return null;
+     
         }
+    
     }
+
 }
 
 ?>
--- test.php
+++ test.php
@@ -2,7 +2,8 @@
 
 require_once 'classes/Task.php';
 
-$task1 = new Task(1,);
-$task2 = new Task(2,3);
+$task1 = new Task(1, );
+
+assert($task1->getStatus('currentStatus') == Task::STATUS_CANCELED, 'canceled');
 
 ?>
