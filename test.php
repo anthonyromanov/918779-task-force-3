15f5d5adc281d610372d3cf934fea19e47634f78
Anton Romanov <609990@gmail.com>
2022-01-11 17:23:55 +0300

    Добавляет правки

--- test.php
+++ test.php
@@ -3,7 +3,8 @@
 require_once 'classes/Task.php';
 
 $task1 = new Task(1, );
+$task2 = new Task(2, 3);
 
-assert($task1->getStatus('currentStatus') == Task::STATUS_CANCELED, 'canceled');
+assert($task1->getStatus('canceled') == Task::STATUS_CANCELED, 'canceled');
 
 ?>
