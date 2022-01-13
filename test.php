38cf2ba492123c5eb85e4e22aff12ddc61fbeb0e
Anton Romanov <609990@gmail.com>
2022-01-13 17:41:19 +0300

    правки

--- test.php
+++ test.php
@@ -2,7 +2,8 @@
 
 require_once 'classes/Task.php';
 
-$task1 = new Task(1, 2);
+$task1 = new Task(1,);
+$task2 = new Task (2, 8);
 
 assert($task1->getMapStatuses()['working'] == 'В работе');
 
@@ -10,8 +11,8 @@ assert($task1->getMapActions()['done'] == 'Выполнено');
 
 assert($task1->getStatusByAction(Task::ACTION_CANCEL) == Task::STATUS_CANCELED);
 
-assert($task1->getAllowedActions(Task::STATUS_NEW,Task::CUSTOMER) == Task::ACTION_CANCEL);
+assert($task1->getAllowedAction(Task::STATUS_NEW,Task::CUSTOMER) == Task::ACTION_CANCEL);
 
-assert($task1->getAllowedActions(Task::STATUS_WORKING,Task::EXECUTOR) == Task::ACTION_REFUSED);
+assert($task1->getAllowedAction(Task::STATUS_WORKING,Task::EXECUTOR) == Task::ACTION_REFUSED);
 
 ?>
