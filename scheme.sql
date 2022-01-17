5ab32a712179d2e12a985594c9d096e2d26e19e5
Anton Romanov <609990@gmail.com>
2022-01-17 17:57:27 +0300

    Правки в схему

--- scheme.sql
+++ scheme.sql
@@ -5,84 +5,76 @@ CREATE DATABASE taskforce
 
 USE taskforce;
 
-CREATE TABLE cities (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(255)
+CREATE TABLE city (
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  title VARCHAR(255) NOT NULL
 );
-CREATE TABLE roles (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(128)
-);
-CREATE TABLE users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  registration DATETIME DEFAULT CURRENT_TIMESTAMP,
-  name VARCHAR(128),
+
+CREATE TABLE user (
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  registration DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  name VARCHAR(128) NOT NULL,
   email VARCHAR(128) NOT NULL UNIQUE,
-  city_id INT,
-  password CHAR(255),
-  role_id INT,
+  city_id INT NOT NULL,
+  password CHAR(255) NOT NULL,
+  role_id INT NOT NULL,
   FOREIGN KEY (city_id) REFERENCES cities(id),
   FOREIGN KEY (role_id) REFERENCES roles(id)
 );
-CREATE TABLE categories (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  code VARCHAR(128) UNIQUE,
-  title VARCHAR(128)
+CREATE TABLE category (
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  code VARCHAR(128) NOT NULL UNIQUE,
+  title VARCHAR(128) NOT NULL
 );
-CREATE TABLE tasks (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  creation DATETIME DEFAULT CURRENT_TIMESTAMP,
-  title VARCHAR(255),
-  description TEXT,
-  estimate INT,
-  runtime DATE,
-  city_id INT,
-  location VARCHAR(255),
-  user_id INT,
-  executor_id INT,
-  category_id INT,
-  task_status VARCHAR(128),
+CREATE TABLE task (
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  creation DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  estimate INT NOT NULL,
+  runtime DATE NOT NULL,
+  city_id INT NOT NULL,
+  location VARCHAR(255) NOT NULL,
+  user_id INT NOT NULL,
+  executor_id INT NOT NULL,
+  category_id INT NOT NULL,
+  task_status VARCHAR(128) NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (category_id) REFERENCES categories(id),
   FOREIGN KEY (city_id) REFERENCES cities(id)
 );
-CREATE TABLE files (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  task_id INT,
-  path VARCHAR(255),
+CREATE TABLE attachment (
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  task_id INT NOT NULL,
+  path VARCHAR(255) NOT NULL,
   FOREIGN KEY (task_id) REFERENCES tasks(id)
 );
-CREATE TABLE executors (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  user_id INT,
+CREATE TABLE executor (
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  user_id INT NOT NULL,
   avatar VARCHAR(255),
   birthday DATE,
-  phone VARCHAR(128),
-  feedback VARCHAR(128),
-  info TEXT,
-  current_status VARCHAR(128),
-  compleded_tasks INT,
-  failed_tasks INT,
+  phone VARCHAR(128) UNIQUE,
   rating INT,
   specialty VARCHAR(255),
   FOREIGN KEY (user_id) REFERENCES users(id)
 );
 CREATE TABLE response (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  task_id INT,
-  executor_id INT,
-  price INT,
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  task_id INT NOT NULL,
+  executor_id INT NOT NULL,
+  price INT NOT NULL,
   comment TEXT,
   FOREIGN KEY (task_id) REFERENCES tasks(id),
   FOREIGN KEY (executor_id) REFERENCES executors(id)
 );
-CREATE TABLE reviews (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  user_id INT,
-  task_id INT,
-  executor_id INT,
-  comment TEXT,
-  stats INT,
+CREATE TABLE review (
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  user_id INT NOT NULL,
+  task_id INT NOT NULL,
+  executor_id INT NOT NULL,
+  comment TEXT NOT NULL,
+  stats INT NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (task_id) REFERENCES tasks(id),
   FOREIGN KEY (executor_id) REFERENCES executors(id)
