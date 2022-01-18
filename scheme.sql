4cb5a5513068a9b4ecac602f605bfbc37dd798f6
Anton Romanov <609990@gmail.com>
2022-01-18 15:07:12 +0300

    Добавляет праки 2.0 в схему

--- scheme.sql
+++ scheme.sql
@@ -14,18 +14,25 @@ CREATE TABLE user (
   id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   registration DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
   name VARCHAR(128) NOT NULL,
+  birthday DATE,
+  avatar VARCHAR(255),
+  phone VARCHAR(11) UNIQUE,
   email VARCHAR(128) NOT NULL UNIQUE,
+  telegram VARCHAR(64) UNIQUE,
+  rating INT,
+  characteristic TEXT,
   city_id INT NOT NULL,
+  specialty VARCHAR(255),
   password CHAR(255) NOT NULL,
-  role_id INT NOT NULL,
-  FOREIGN KEY (city_id) REFERENCES cities(id),
-  FOREIGN KEY (role_id) REFERENCES roles(id)
-);
+  FOREIGN KEY (city_id) REFERENCES city(id),
+);  
+
 CREATE TABLE category (
   id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   code VARCHAR(128) NOT NULL UNIQUE,
   title VARCHAR(128) NOT NULL
 );
+
 CREATE TABLE task (
   id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   creation DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
@@ -34,49 +41,40 @@ CREATE TABLE task (
   estimate INT NOT NULL,
   runtime DATE NOT NULL,
   city_id INT NOT NULL,
-  location VARCHAR(255) NOT NULL,
+  lat DECIMAL(10, 8) NOT NULL,
+  lng DECIMAL(10, 8) NOT NULL,
   user_id INT NOT NULL,
-  executor_id INT NOT NULL,
   category_id INT NOT NULL,
   task_status VARCHAR(128) NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users(id),
-  FOREIGN KEY (category_id) REFERENCES categories(id),
-  FOREIGN KEY (city_id) REFERENCES cities(id)
+  FOREIGN KEY (user_id) REFERENCES user(id),
+  FOREIGN KEY (category_id) REFERENCES category(id),
+  FOREIGN KEY (city_id) REFERENCES city(id)
 );
+
 CREATE TABLE attachment (
   id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   task_id INT NOT NULL,
   path VARCHAR(255) NOT NULL,
-  FOREIGN KEY (task_id) REFERENCES tasks(id)
+  FOREIGN KEY (task_id) REFERENCES task(id)
 );
-CREATE TABLE executor (
-  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  user_id INT NOT NULL,
-  avatar VARCHAR(255),
-  birthday DATE,
-  phone VARCHAR(128) UNIQUE,
-  rating INT,
-  specialty VARCHAR(255),
-  FOREIGN KEY (user_id) REFERENCES users(id)
-);
+
 CREATE TABLE response (
   id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   task_id INT NOT NULL,
-  executor_id INT NOT NULL,
+  user_id INT NOT NULL,
   price INT NOT NULL,
   comment TEXT,
-  FOREIGN KEY (task_id) REFERENCES tasks(id),
-  FOREIGN KEY (executor_id) REFERENCES executors(id)
+  FOREIGN KEY (task_id) REFERENCES task(id),
+  FOREIGN KEY (user_id) REFERENCES user(id)
 );
+
 CREATE TABLE review (
   id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   user_id INT NOT NULL,
   task_id INT NOT NULL,
-  executor_id INT NOT NULL,
   comment TEXT NOT NULL,
   stats INT NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users(id),
-  FOREIGN KEY (task_id) REFERENCES tasks(id),
-  FOREIGN KEY (executor_id) REFERENCES executors(id)
+  FOREIGN KEY (user_id) REFERENCES user(id),
+  FOREIGN KEY (task_id) REFERENCES task(id),
 );
   
