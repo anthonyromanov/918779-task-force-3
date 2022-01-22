f095fe7dc1ac7565af8433d5d00765f855de6d20
Anton Romanov <609990@gmail.com>
2022-01-22 16:05:21 +0300

    Правки последние

--- src/DefaultAction.php
+++ src/DefaultAction.php
@@ -6,22 +6,10 @@ abstract class DefaultAction {
 
     private $idUser;
 
-    private function setIdCustomer ($idUser, $idCustomer) {
-
-        $this->idCustomer = $idUser;
-
-    }
-
-    private function setIdExecutor ($idUser, $idExecutor) {
-
-        $this->idExecutor = $idUser;
-
-    }
-
     public function __construct($idExecutor, $idCustomer = null) {
 
-        $this->setIdCustomer($idCustomer);
-        $this->setIdExecutor($idExecutor);
+        $this->idExecutor = $idExecuror;
+        $this->idCustomer = $idCustomer;
 
     }
 
