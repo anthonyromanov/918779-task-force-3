65666d6a4fc7a19e2952f1b7190653214dcbcc21
Anton Romanov <609990@gmail.com>
2022-02-10 13:42:26 +0300

    Армия моделей

---
+++ models/Category.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "category".
+ *
+ * @property int $id
+ * @property string $icon
+ * @property string $name
+ *
+ * @property Task[] $tasks
+ */
+class Category extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'category';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['icon', 'name'], 'required'],
+            [['icon', 'name'], 'string', 'max' => 128],
+            [['icon'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'icon' => 'Icon',
+            'name' => 'Name',
+        ];
+    }
+
+    /**
+     * Gets query for [[Tasks]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTasks()
+    {
+        return $this->hasMany(Task::className(), ['category_id' => 'id']);
+    }
+}
